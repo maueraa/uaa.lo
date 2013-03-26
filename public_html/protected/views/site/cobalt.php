e3f913e30b31e234980043534726f3b66b279b1b
Sarvarbek Qo'ldashev <s.kuldashev@uzautoaustem.uz>
2013-03-26 20:24:41 +0500

    Cobal  materiallarini kiritdim
    Signed-off-by: Sarvarbek Qo'ldashev <s.kuldashev@uzautoaustem.uz>

--- public_html/protected/views/site/cobalt.php
+++ public_html/protected/views/site/cobalt.php
@@ -4,15 +4,30 @@
 $this->pageTitle=Yii::app()->name;
 include_once("car_menus.php");
 ?>
+<style type="text/css">
+<!--
+.style1 {font-family: Arial, Helvetica, sans-serif}
+.style6 {font-size: 12px}
+.style7 {font-family: Arial, Helvetica, sans-serif; font-size: 12px; }
+-->
+</style>
+
 <div class="column-center">
     <div class="custom">
         <div class="box2 block01">
             <div class="titlebox">
                 <h2><?=Yii::t("strings","Кобалт")?></h2>
             </div>
+
+
+
             <div class="text-box" align="center">
                 O'z Auto Austem қўшма корхонаси Кобалт автомобиллари учун турли хил кўринишлардаги дискларни ишлаб чиқаради</p>
             </div>
+
+
+
+
         </div>
         <div class="block03">
             <img src="images/header.jpg" class="fasad" width="442px" height="170">-->
@@ -25,10 +40,59 @@ include_once("car_menus.php");
                 <div class="title">
                     <h2><a href="#" rel="bookmark">O'z Auto Austem</a></h2><br>
                 </div>
+
+
+
+
                 <div class="text-box">
-					
+
+                    <table width="100%">
+                        <tr>
+                            <td width="218" align="center" valign="middle">
+                                <div align="left"><img border="0" src="images/frameasm.png" width="189" height="86"></div></td>
+                            <td width="810" align="center" valign="middle">
+                                <p align="center" class="MsoNormal style1 style6" style="text-align: center"><b>
+		<span lang="EN-US" style="line-height: 115%">
+		FRAME ASM <span style="color: #9999FF">-&nbsp; </span>DRIVETRAIN &amp; FRT SUSP</span></b></p>
+                                <p align="center" class="MsoNormal style1 style6" style="text-align: center">
+                                    <b>
+		                            <span lang="EN-US" style="line-height: 115%">Material
+		spec: GMW3032M-ST-S HR 420 LA</span></b></p>
+                          <p class="style7"></td>
+                            <td width="65" align="center" valign="middle"><b><font size="4">$95,00</font></b></td>
+                        </tr>
+                        <tr>
+                            <td width="218" align="center" valign="middle"><div align="left"></div></td>
+                            <td width="810" align="center" valign="middle"><span class="style6"></span></td>
+                            <td align="center" valign="middle">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td width="218" align="center" valign="middle">
+                                <div align="left"><img border="0" src="images/rearaxle2.png" width="204" height="76"></div></td>
+                            <td width="810" align="center" valign="middle">
+                                <p align="center" class="MsoNormal style1 style6" style="text-align: center"><b>
+		<span lang="EN-US" style="line-height: 115%">
+		REAR AXLE WELDMENT ASSEMBLY</span></b></p>
+                                <p align="center" class="MsoNormal style1 style6" style="text-align: center">
+                                    <b>
+		                            <span lang="EN-US" style="line-height: 115%">Material
+		spec: GMW 3032 M-ST-S HR 550</span></b></p>
+                          <p class="style7"></td>
+                            <td align="center" valign="middle"><b><font size="4">$175,00</font></b></td>
+                        </tr>
+                        <tr>
+                            <td align="center">&nbsp;</td>
+                            <td align="center">&nbsp;</td>
+                            <td align="center">&nbsp;</td>
+                        </tr>
+                    </table>
+
 					<div class="link-edit"></div>
                 </div>
+
+
+
+
             </div>
             <div class="post-1 post hentry category-etiam-cursus-leo category-quisque-nulla-vestibu category-malesuad-neque-vivamus category-orci-luctus-et-ultrices category-primis-in-faucibus tag-ipsa tag-sed" id="post-1">
             </div>
